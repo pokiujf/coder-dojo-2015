b3fcb8f283f7528b4b6c02eb199fc2e1aeefd31f
pokiujf <arkadiusz.machalica@gmail.com>
2015-07-21 20:01:53 +0000

    Remove chars - refactoring

--- code_eval_13/remove_chars.rb
+++ code_eval_13/remove_chars.rb
@@ -1,6 +1,4 @@
-require '../support/process_file'
-
-ProcessFile.new do |line|
+File.open(ARGV[0], "r").each_line do |line| 
   sentence, chars = line.split(', ')
   puts sentence.gsub(Regexp.new(chars.split('').join('|')), '')
 end
