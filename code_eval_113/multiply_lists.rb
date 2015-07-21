f411b877eb7695e58145bd4053dc9853c947a83a
pokiujf <arkadiusz.machalica@gmail.com>
2015-07-21 20:42:25 +0000

    Multiply lists - refactor to two lines

--- code_eval_113/multiply_lists.rb
+++ code_eval_113/multiply_lists.rb
@@ -1,12 +1,6 @@
 require '../support/process_file'
 
-ProcessFile.new do |list|
-  list1, list2 = list.strip.split(' | ')
-  list1 = list1.split.map(&:to_i)
-  list2 = list2.split.map(&:to_i)
-  combined = []
-  list1.each_with_index do |val, index|
-    combined[index] = val * list2[index]
-  end
-  puts combined.join(' ')
+ProcessFile.new do |lists|
+  list1, list2 = lists.strip.split(' | ').map{|list| list.split.map(&:to_i)}
+  puts list1.zip(list2).map{|sub_arr| sub_arr.reduce(:*)}.flatten.join(' ')
 end
