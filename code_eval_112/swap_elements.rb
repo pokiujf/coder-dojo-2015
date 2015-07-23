7e3ced58a86bd566a8f9a60126c400f924bcd7a3
pokiujf <arkadiusz.machalica@gmail.com>
2015-07-23 09:08:56 +0000

    Swap elements - working solution

--- code_eval_112/swap_elements.rb
+++ code_eval_112/swap_elements.rb
@@ -4,6 +4,13 @@ ProcessFile.new do |line|
   data, operations = line.strip.split(' : ')
   data = data.split(' ')
   operations = operations.split(', ').map{|operation| operation.split('-').map(&:to_i)}
-  puts "data: #{data}"
-  puts "operations: #{operations}"
+  # puts "data: #{data}"
+  # puts "operations: #{operations}"
+  operations.each do |operation|
+    first_digit =  data[operation.first]
+    last_digit = data[operation.last]
+    data[operation.first] = last_digit
+    data[operation.last] = first_digit
+  end
+  puts data.join(' ')
 end
