2e7b46cd777b8eb61d872d0e7981c07e9b275b86
pokiujf <arkadiusz.machalica@gmail.com>
2015-08-08 19:00:05 +0000

    Detect loops - refactor with commentary

--- code_eval_5/detect_loops.rb
+++ code_eval_5/detect_loops.rb
@@ -14,23 +14,36 @@ class LoopDetecter
   private
   
   def detect
-    sequence_length = @sequence.length
-    max_length = 50 < sequence_length / 2 ? 50 : sequence_length / 2
-    loop_lengths = (1..max_length)
-    loop_lengths.each do |loop_length|
+    # sequence_length / 2 because there is no need to search for loops
+    # that are longer than half of the sequence
+    (1..sequence_length / 2).each do |loop_length|
+      
+      # according to spec max loop length is 50 so no need to serach longer
+      break if loop_length > 50
+      
+      # sequence_length - loop_length * 2 because there is no need to search for
+      # n_num loop if thre is less than n numbers to the end of sequence
       (0..sequence_length - loop_length * 2).each do |starting_index|
-        # puts "first #{@sequence[starting_index, loop_length]}, second #{@sequence[starting_index + loop_length, loop_length]}"
-        if @sequence[starting_index, loop_length] == @sequence[starting_index + loop_length, loop_length]
-          # puts "DETECTED"
+        
+        
+        if loop_exist?(starting_index, loop_length)
+          # puts "first #{@sequence[starting_index, loop_length]}, second #{@sequence[starting_index + loop_length, loop_length]}"
           return @sequence[starting_index, loop_length]
         end
       end
     end
   end
   
+  def loop_exist?(start, length)
+    @sequence[start, length] == @sequence[start + length, length]
+  end
+  
+  def sequence_length
+    @sequence_length ||= @sequence.length
+  end
 end
 
 ProcessFile.new(filename) do |line|
   sequence = line.split(' ').map(&:to_i)
-  puts LoopDetecter.new(sequence).to_s
+  puts LoopDetecter.new(sequence)
 end
